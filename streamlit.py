db5c9ca8b3e7c1f6a8432a935b97e08ab849c5e2
María Sanz <sanzrocamoramaria@gmail.com>
2023-12-06 15:22:09 +0100

    borrador

--- streamlit.py
+++ streamlit.py
@@ -14,12 +14,12 @@ from datetime import datetime
 
 # Page configuration
 st.set_page_config(
-    page_title = "Book recommendation system",
+    page_title = "BookBuddy",
     page_icon = '📚',
     layout = 'wide',
     initial_sidebar_state = 'expanded',
 )
-st.title("Book recommendation system")
+st.title("BookBuddy")
 
 # Data loading
 client = pymongo.MongoClient(mongo_db_url)
@@ -37,39 +37,71 @@ st.sidebar.header('Filters')
 unique_authors = df['author'].unique()
 unique_authors = ['Choose an option'] + list(unique_authors)
 selected_author = st.sidebar.selectbox('Author', unique_authors)
-if selected_author != 'Select an author':
+if selected_author != 'Choose an option':
+    filtered_df = df[df['author'] == selected_author]
+    st.write(filtered_df)
     st.write(f"Author selected: {selected_author}")
+else:
+    st.write("No author selected")
 
-# Filtro para el género
+# Filter for the genre
 df['genre'] = df['genre'].apply(lambda x: ast.literal_eval(x))
 unique_genres = set()
 for value in df['genre']:
     unique_genres.update(value.values())
 selected_genres = st.sidebar.multiselect('Genre', list(unique_genres))
 filtered_df = df[df['genre'].apply(lambda x: any(item in selected_genres for item in x.values()))]
-
-
-# Filtro para la fecha de publicación
-selected_date = st.sidebar.date_input('Publication date')
-
-# Extraer el año de la fecha seleccionada
-if selected_date:
-    selected_year = datetime.strptime(str(selected_date), "%Y-%m-%d").year
-    st.write(f"Year selected: {selected_year}")
+if selected_genres:
+    st.write(filtered_df)
 else:
-    st.write("No date selected")
+    st.write("No genres selected")
+
+
+# Filter for the publication year
+years = sorted(df['publication_date'].dt.year.unique())
+years.insert(0, '')
+selected_year = st.sidebar.select_slider('Publication year', options=years)
+if selected_year != '':
+    filtered_df = df[df['publication_date'].dt.year == selected_year]
+    st.write(filtered_df)
+else:
+    st.write("No year selected")
 
 # Filtro para la calificación
 min_rating, max_rating = st.sidebar.slider('Rating', 0.0, 5.0, (0.0, 5.0))
+if min_rating != 0.0 or max_rating != 5.0:
+    filtered_df = df[(df['rating'] >= min_rating) & (df['rating'] <= max_rating)]
+    if not filtered_df.empty:
+        st.write(filtered_df)
+    else:
+        st.write("No matching data")
+else:
+    st.write("No rating filter applied")
 
 # Filtro para el número de ratings
 min_num_ratings = df['rating_count'].min()
 max_num_ratings = df['rating_count'].max()
 
+# Filtro para el número de ratings con valores por defecto que abarquen todo el rango
 selected_min, selected_max = st.sidebar.slider('Number of ratings', min_num_ratings, max_num_ratings, (min_num_ratings, max_num_ratings))
+if (selected_min, selected_max) != (min_num_ratings, max_num_ratings):
+    filtered_df = df[(df['rating_count'] >= selected_min) & (df['rating_count'] <= selected_max)]
+    if not filtered_df.empty:
+        st.write(filtered_df)
+    else:
+        st.write("No matching data")
+else:
+    st.write("No number of ratings filter applied")
 
-# Filtro para el número de reviews
+# Filtro para el número de paginas
 min_num_pages = df['num_page'].min()
 max_num_pages = df['num_page'].max()
-
-selecting_min, selecting_max = st.sidebar.slider('Number of pages', min_num_pages, max_num_pages, (min_num_pages, max_num_pages))
+selected_min, selected_max = st.sidebar.slider('Number of pages', min_num_pages, max_num_pages, (min_num_pages, max_num_pages))
+if (selected_min, selected_max) != (min_num_pages, max_num_pages):
+    filtered_df = df[(df['num_page'] >= selected_min) & (df['num_page'] <= selected_max)]
+    if not filtered_df.empty:
+        st.write(filtered_df)
+    else:
+        st.write("No matching data")
+else:
+    st.write("No number of pages filter applied")
